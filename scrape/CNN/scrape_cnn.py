c47ddf258497d0c29ec6c63f0cf196c9b8075860
Tram La <latb@uci.edu>
2023-03-11 00:29:44 -0800

    set up scrape for cnn

--- scrape/CNN/scrape_cnn.py
+++ scrape/CNN/scrape_cnn.py
@@ -14,15 +14,21 @@ def scrape_cnn(url):
     soup = request_website(url)
 
     article = Article("CNN")
+    article.set_url(url)
 
     ## get title headline
-    article.set_headline(soup.find("h1", class_="headline__text inline-placeholder").text)
+    try:
+        article.set_headline(soup.find("h1", class_="headline__text inline-placeholder").text)
+    except:
+        print("Not article")
+        return article
 
     ## get author
     try:
         article.set_author(soup.find("span", class_="byline__name").text)
     except:
         print("No Author")
+
     
     ## get date
     date = soup.find("div", class_="timestamp").text
@@ -32,10 +38,17 @@ def scrape_cnn(url):
                                year=int(all_info[-1]))
 
     ## get text
-    raw_text = soup.find("div", class_="article__content-container")
-    content = raw_text.find_all("p")
-    article.set_content([p.text.strip() for p in content])
 
+    raw_text = soup.find("div", class_="article__content-container")
+    if not raw_text:
+        raw_text = soup.find("div", class_="article__content")
+    
+    try:
+        content = raw_text.find_all("p")
+        article.set_content([p.text.strip() for p in content])
+    except:
+        print("faulty article")
+        return article
     return article
 
 
@@ -49,7 +62,7 @@ def get_links(year, main_bias):
 
 if __name__ == "__main__":
 
-    MAIN_BIAS, YEAR = "immigration", 2011
+    MAIN_BIAS, YEAR = "immigration", 2021
 
     information = get_links(YEAR, MAIN_BIAS)
     scrape_info = {"Biases": information["Biases"], "Articles": []}
@@ -60,3 +73,4 @@ if __name__ == "__main__":
 
     full_write_path = PATH_WRITE + MAIN_BIAS + '_' + str(YEAR) + '.json'
     write_to_json(full_write_path, scrape_info)
+
