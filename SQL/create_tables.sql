b582240500e90b1bb7ac63de20ebec8c5a6050fc
Lucy Lu <junyil16@uci.edu>
2023-04-15 16:19:59 -0700

    Added insert statement for bias and media tables

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -38,3 +38,14 @@ CREATE TABLE elections(
     PRIMARY KEY(candidate_name, election_year)
     
 );
+
+INSERT INTO biases (main_bias, sub_bias) VALUES ('immigration', ARRAY['immigration','undocumented','refugees','nationalism','border','Dreamers', 'asylum seekers','xenophobia']);
+INSERT INTO biases(main_bias, sub_bias) VALUES ('racial', ARRAY['racial', 'disparities', 'white privilege', 'black lives matter', 'segregation', 'critical race theory', 'criminal', 'racial discrimination', 'CRT', 'woke', 'wokeness']);
+INSERT INTO biases(main_bias, sub_bias) VALUES ('Abortion', ARRAY['Abortion', 'Pro-life', 'pro-choice', 'consent', 'mifepristone', 'abortion pill', 'planned parenthood','rape','reproductive rights','women’s right','reproductive rights','abortion bans','birth control']);
+INSERT INTO biases(main_bias, sub_bias) VALUES ('socioeconomic', ARRAY['socioeconomic','poverty line','class privilege','working class','pro-public assistance','marginalized','middle class','poverty','food stamps','medicare','medical','rich','wealthy']);
+
+
+
+INSERT INTO media_source(source_name) VALUES('FOX');
+INSERT INTO media_source(source_name) VALUES('NPR');
+INSERT INTO media_source(source_name) VALUES('CBS');
