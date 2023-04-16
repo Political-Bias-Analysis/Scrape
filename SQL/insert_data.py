0199ba31f1fa676e6eaea731ec6dfa7804b4dd70
Lucy Lu <junyil16@uci.edu>
2023-04-15 21:16:45 -0700

    Added code for article visualization

--- SQL/insert_data.py
+++ SQL/insert_data.py
@@ -1,13 +1,13 @@
 import psycopg2
 import json
 
-def insert_data(info, main_bias, sub_bias):
+def insert_data(info):
 
     #establish connection with the database called senior project
     con = psycopg2.connect(
         database= "senior_project",
         user= "postgres",
-        password= "your password",
+        password= "1668",
         host= "localhost",
         port= "5433"
         )
@@ -18,22 +18,24 @@ def insert_data(info, main_bias, sub_bias):
     #To execute any postgre query, we need to use execute function
     #insert data into biases table
     #cursor_obj.execute("INSERT INTO biases (main_bias, sub_bias) VALUES (%s, %s);", (info["Biases"][0], info["Biases"]))
-    cursor_obj.execute("INSERT INTO biases (main_bias, sub_bias) VALUES (%s, %s);", (main_bias, sub_bias))
+    #cursor_obj.execute("INSERT INTO biases (main_bias, sub_bias) VALUES (%s, %s);", (main_bias, sub_bias))
 
     #insert data into articles table
     for i in range(len(info["Articles"])):
-        cursor_obj.execute("INSERT INTO articles (headline, author, source, published_date, article_content, bias, url) VALUES(%s, %s, %s, %s, %s, %s, %s);", (info["Articles"][i]["headline"], info["Articles"][i]["author"], info["Articles"][i]["source"],info["Articles"][i]["published_date"]["month"] + "/" + str(info["Articles"][i]["published_date"]["day"]) + "/" + str(info["Articles"][i]["published_date"]["year"]), info["Articles"][i]["article_content"], info["Biases"], info["Articles"][i]["url"]))
+        if info["Articles"][i]["published_date"]["month"] != "" or info["Articles"][i]["published_date"]["year"] != 0 or info["Articles"][i]["published_date"]["year"] != 0:
+
+            cursor_obj.execute("INSERT INTO articles (headline, author, source, published_date, article_content, bias, url) VALUES(%s, %s, %s, %s, %s, %s, %s);", (info["Articles"][i]["headline"], info["Articles"][i]["author"], info["Articles"][i]["source"],info["Articles"][i]["published_date"]["month"] + "/" + str(info["Articles"][i]["published_date"]["day"]) + "/" + str(info["Articles"][i]["published_date"]["year"]), info["Articles"][i]["article_content"], info["Biases"], info["Articles"][i]["url"]))
 
     #Insert data into   
-    cursor_obj.execute("INSERT INTO media_source (source_name) VALUES (%s);", (str(info["Articles"][0]["source"]),))
+    #cursor_obj.execute("INSERT INTO media_source (source_name) VALUES (%s);", (str(info["Articles"][0]["source"]),))
  
     con.commit()
 
 
 if __name__ == "__main__":
-    path = "/Users/lucylu/Senior-Project/data/articles/CNN/immigration_2011.json"
+    path = "/Users/lucylu/Desktop/Senior-Project/data/articles/FOX/immigration_2020.json"
     with open(path, 'r') as f:
         dictionary = json.load(f)
     main_bias = "immigration"
     sub_bias = ["immigration","undocumented","refugees","nationalism","border", "Dreamers", "asylum seekers","xenophobia"]
-    insert_data(dictionary, main_bias, sub_bias)
+    insert_data(dictionary)
