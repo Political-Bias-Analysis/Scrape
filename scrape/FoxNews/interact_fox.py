e26e4059c540f4cbfbebf4fe32a9e18cfd76dd38
Tram La <latb@uci.edu>
2023-04-24 14:10:17 -0700

    clean code

--- scrape/FoxNews/interact_fox.py
+++ scrape/FoxNews/interact_fox.py
@@ -7,6 +7,9 @@ import sys
 sys.path.append("/Users/tramla/Desktop/UCI Courses/Senior-Project/Scrape/scrape/")
 from ReadWriteFiles.read_write_links import *
 
+## TRAM'S PATH
+PATH = "/Users/tramla/Desktop/UCI Courses/Senior-Project/Data/data"
+
 def access_websites(bias, dates, all_links):
     driver = webdriver.Chrome()
     driver.get('https://www.foxnews.com/search-results/search?q=election')
@@ -69,7 +72,7 @@ def access_websites(bias, dates, all_links):
     time.sleep(2)
     for elem in articles:
         link = elem.find_element(By.TAG_NAME, 'a').get_attribute('href')
-        if check_us_news(link) and link not in all_links: 
+        if check_us_news(link): #and link not in all_links: 
             links.append({"link": link, "bias": bias})
             all_links.add(link)
     
@@ -84,18 +87,16 @@ def check_us_news(url):
 
 
 def write_links_new_bias(links_info, bias, year):
-    PATH = '../../data/links/FOX/' + bias + '_' + str(year) + '.json'
-    with open(PATH, 'w+') as f:
+    with open(PATH + '/links/FOX/' + bias + '_' + str(year) + '.json', 'w+') as f:
         output = json.dumps(links_info, indent=4)
         f.write(output)
 
 
 
 def write_links_exist_bias(main_bias, bias, year, links):
-    PATH = '../../data/links/FOX/' + main_bias + '_' + str(year) + '.json'
     
     cur = None
-    with open(PATH, 'r') as f:
+    with open(PATH + '/links/FOX/' + main_bias + '_' + str(year) + '.json', 'r') as f:
         cur = json.loads(f.read())
 
     cur["Biases"].append(bias)
@@ -115,7 +116,7 @@ if __name__ == "__main__":
     ## run this: if new bias, change MAIN_BIAS and EXISTS = false, else only change bias and keep EXISTS = True
     MAIN_BIAS, EXISTS = "immigration", True
     biases = ["immigration", "undocumented", "refugees", "asylum seekers", "nationalism", "border", "Dreamers", "xenophobia"]
-    year, bias = 2020, biases[1]
+    year, bias = 2020, biases[7]
     
     all_links = get_all_links(MEDIA_NAME)
     
